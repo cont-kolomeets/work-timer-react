fb85873955f94183e624f4539091786521876eb9
Alexander Kolomeets <akolomeets@dataeast.com>
2024-03-21 16:14:06 +0700

    fix focus issue

--- src/shared/lib/focusUtil.ts
+++ src/shared/lib/focusUtil.ts
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useInputFocus() {
   const [element, setElement] = useState<HTMLInputElement | null>(null);
   const setFocus = () => {
     element?.focus();
     element?.select();
+    return !!element;
   };
   return { element, setElement, setFocus };
 }
 
 export function useInputFocusOnCreate() {
   const { element, setFocus, setElement } = useInputFocus();
+  let focused = useRef(false);
   useEffect(() => {
-    setFocus();
-  }, [element, setFocus]);
+    if (!focused.current) {
+      focused.current = setFocus();
+    }
+  }, [element, setFocus, focused]);
   return { setElement };
 }
